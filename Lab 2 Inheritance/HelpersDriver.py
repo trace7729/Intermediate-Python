1da009b19ac5f365ac11ba519ee1429179dcde34
trace7729 <33745132+trace7729@users.noreply.github.com>
2020-02-13 04:48:13 -0800

    Add files via upload

---
+++ Lab 2 Inheritance/HelpersDriver.py
@@ -0,0 +1,8 @@
+# Python III Lab 2 Annie Yen
+
+from Helpers import *
+
+print(inputInt("Please enter a number: ", 0, 100))
+print(inputFloat("Please enter a number: ", -10, 1000))
+print(inputString("Enter some text: ", 5, 10))
+print(inputDate("Enter a date: "))
